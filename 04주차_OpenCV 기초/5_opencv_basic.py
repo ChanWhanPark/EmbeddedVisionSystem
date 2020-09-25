d094e58786eec5addcb3f94b505088c9bcd0a2e5
ChanWhanPark <chananpark@github.com>
2020-09-25 10:14:28 +0900

    2020.09.25(5.py 주석 추가)

--- 04주차_OpenCV 기초/5_opencv_basic.py
+++ 04주차_OpenCV 기초/5_opencv_basic.py
@@ -11,7 +11,7 @@ cv2.waitKey(0)
 cv2.imwrite('result1.png', img_basic) # 이미지 파일 저장
 
 img_gray = cv2.cvtColor(img_basic, cv2.COLOR_BGR2GRAY) # 흑백 사진으로 바꿈
-cv2.imshow('Image Gray', img_gray)
+cv2.imshow('Image Gray', img_gray) # 이름을 같게 하면 화면이 바뀜
 cv2.waitKey(0)
 
 cv2.imwrite('result2.png', img_gray)
