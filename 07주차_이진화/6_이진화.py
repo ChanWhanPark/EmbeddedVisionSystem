b17e853294d4706d002439bd2966e4b3071d4c11
ChanWhanPark <chananpark@github.com>
2020-11-06 09:40:05 +0900

    2020.10.23(수정)

--- 07주차_이진화/6_이진화.py
+++ 07주차_이진화/6_이진화.py
@@ -2,7 +2,7 @@ import cv2 as cv
 
 img = cv.imread('Lenna.jpg', cv.IMREAD_GRAYSCALE)
 
-ret, mask = cv.threshold(img, 128, 255, cv.THRESH_BINARY)
+ret, mask = cv.threshold(img, 128, 256, cv.THRESH_BINARY)
 cv.imshow("Original Image", img);
 cv.imshow("New Image", mask);
 
